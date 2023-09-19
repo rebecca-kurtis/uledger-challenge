c328bc9c7b50476195fdfa29327001325b5d44d4
Jacky Ngo <jacky.ngo96@gmail.com>
2023-09-19 15:34:45 -0400

    create login page route

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoginPage : React.FC = () => {
+  return (
+    <div className="flex justify-end justify-col container mx-auto mt-8"></div>
+  );
+};
+
+export default LoginPage;
