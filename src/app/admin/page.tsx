482c7593d10a9737a2dcef9c01a765bc9e719d52
Jacky Ngo <jacky.ngo96@gmail.com>
2023-09-20 22:55:52 -0400

    create admin dashboard page

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AdminDashboard: React.FC = () => {
+  return (
+    <div>
+      <h1 className="mt-8 text-4xl text-gray-700 font-bold mb-2">
+        Admin Dashboard:
+      </h1>
+    </div>
+  );
+};
+
+export default AdminDashboard;
